9f06d1ac50bdcb3358fc306cf20408f21435503e
leonardozcm <597714572@qq.com>
2021-07-22 10:16:19 +0000

    ipex build finnished

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 if [ `uname` == Linux ]; then
-    if [ "$PY_VER" == "3.9" ]; then
-        pip install https://github.com/leonardozcm/intel-extension-for-pytorch/releases/download/v1.8.0/torch_ipex-1.8.0-cp39-cp39-linux_x86_64.whl
+    if [ "$PY_VER" == "3.8" ]; then
+        pip install https://github.com/leonardozcm/intel-extension-for-pytorch/releases/download/v1.8.0/torch_ipex-1.8.0-cp38-cp38-linux_x86_64.whl --no-deps
     fi
 fi
